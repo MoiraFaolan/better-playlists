0fe5634036b667efdc10f0b48fd3f83e33423828
MoiraFaolan <37768237+MoiraFaolan@users.noreply.github.com>
2018-03-25 21:44:00 +1100

    Cleaned up formatting

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-let defaultTextColor = '#999'
 let defaultStyle = {
-  color:defaultTextColor
+  color: '#666'
 }
+
 class Aggregate extends Component {
   render () {
     return (
       <div style={{...defaultStyle, width: "40%", display: 'inline-block'}}>
         <h2>Number Text</h2>
       </div>
-
     );
   }
 }
@@ -45,7 +43,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Title</h1>
+        <h1 style={{...defaultStyle, 'font-size': '54px'}}>Title</h1>
         <Aggregate/>
         <Aggregate/>
         <Filter/>
