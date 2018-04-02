2420d68380bee08de0b16f25ab82070a7464dbdc
MoiraFaolan <37768237+MoiraFaolan@users.noreply.github.com>
2018-04-02 20:12:25 +1000

    Prettify

--- src/App.js
+++ src/App.js
@@ -1,30 +1,26 @@
 import React, { Component } from 'react';
+import 'reset-css/reset.css';
 import './App.css';
 import queryString from 'query-string'; 
 
 let defaultStyle = {
-  color: '#fff'
+  color: '#fff',
+  'font-family': 'Segoe UI Light'
 };
-let fakeServerData = {
-  user: {
-    name: 'Tasha',
-    playlists: [
-      {
-        name: 'My favourites',
-        songs: [
-          {name: 'Beat It', duration: 1345}, 
-          {name: 'two', duration: 1236},
-          {name: 'Beat three', duration: 70000} 
-        ]
-      }
-    ]
-  }
-};
+
+let counterStyle = {...defaultStyle, 
+  width: "40%", 
+  display: 'inline-block',
+  'margin-bottom': '20px',
+  'font-size': '20px',
+  'line-height': '30px'
+}
 
 class PlaylistCounter extends Component {
   render () {
+    let playlistCounterStyle = counterStyle
     return (
-      <div style={{...defaultStyle, width: "40%", display: 'inline-block'}}>
+      <div style={playlistCounterStyle}>
         <h2>{this.props.playlists.length} playlists</h2>
       </div>
     );
@@ -39,9 +35,16 @@ class HoursCounter extends Component {
     let totalDuration = allSongs.reduce((sum, eachSong) => {
       return sum + eachSong.duration
     }, 0)
+    let totalDurationHours = Math.round(totalDuration/60/60)
+    let isTooLow = totalDurationHours < 1
+    let hoursCounterStyle = {...counterStyle, 
+      color: isTooLow ? 'red' : 'white',
+      'font-weight': isTooLow ? 'bold' : 'normal'
+
+    }    
     return (
-      <div style={{...defaultStyle, width: "40%", display: 'inline-block'}}>
-        <h2>{Math.round(totalDuration/60/60)} hours</h2>
+      <div style={hoursCounterStyle}>
+        <h2>{totalDurationHours} hours</h2>
       </div>
     );
   }
@@ -52,8 +55,14 @@ class Filter extends Component {
     return (
       <div style={defaultStyle}>
         <img/>
-        <input type="text" onKeyUp={event => 
-          this.props.onTextChange(event.target.value)}/>
+        <input type="text" placeholder="Filter playlists..." onKeyUp={event => 
+          this.props.onTextChange(event.target.value)}
+          style={{...defaultStyle,
+            color: 'black',
+            padding: '10px', 
+            'margin-bottom': '20px',
+            'border-radius': '25px', 
+            'border': 'none'}}/>
       </div>
     );
   }
@@ -63,15 +72,23 @@ class Playlist extends Component {
   render () {
     let playlist = this.props.playlist
     return (
-      <div style={{...defaultStyle, width: "25%", display: 'inline-block'}}>
-        <img src={playlist.imageUrl} style={{width: '60px'}}/>
-        <h3>{playlist.name}</h3>
-        <ul>
-          {playlist.songs.map(song =>
-            <li>{song.name}</li>
-          )}
-        </ul>
-      </div>
+        <div style={{...defaultStyle, 
+          display: 'inline-block',
+          width: '25%',
+          padding: '10px',
+          'background-color': this.props.index % 2 
+            ? '#808080' 
+            : '#1db954'
+          }}>
+          <img src={playlist.imageUrl} style={{width: '100px'}}/>
+          <h3 style={{'font-weight': 'bold'}}>{playlist.name}</h3>
+          <ul style={{'margin-top': '10px'}}>
+            {playlist.songs.map(song =>
+              <li style={{'padding-top': '2px'}}>{song.name}</li>
+            )}
+          </ul>
+        </div>
+
     );
   }
 }
@@ -154,7 +171,11 @@ class App extends Component {
       <div className="App">
         {this.state.user ? 
         <div>
-          <h1 style={{...defaultStyle, 'font-size': '54px'}}>
+          <h1 style={{...defaultStyle, 
+            'font-size': '54px',
+            'margin-top': '5px',
+            'margin-bottom': '20px'
+            }}>
             {this.state.user.name}'s Playlists
           </h1>
           <PlaylistCounter playlists={playlistsToRender}/>
@@ -162,15 +183,27 @@ class App extends Component {
           <Filter onTextChange={text => {
             this.setState({filterString: text})
             }}/>
-          {playlistsToRender.map(playlist  => 
-            <Playlist playlist={playlist}/>
+          {playlistsToRender.map((playlist, i)  => 
+            <Playlist playlist={playlist} index={i}/>
           )}
-        </div> : <button onClick={() => {
+        </div> : 
+        <div style={{'display': 'grid', 'align-self': 'center', 'justify-self': 'center', 'height': '50px'}}>
+          <button onClick={() => {
             window.location = window.location.href.includes('localhost') 
               ? 'http://localhost:8888/login'
               : 'https://better-playlists-backend-mf.herokuapp.com/login'}
           }
-          style={{padding: '20px', 'font-size': '50px', 'margin-top': '20px'}}>Sign in with Spotify</button>
+          style={{...defaultStyle,
+            padding: '10px', 
+            'font-size': '20px',
+            'font-weight': 'bold', 
+            'margin-top': '40px',
+            'border-radius': '25px',
+            'background-color': '#1db954',
+            'border': 'none',
+            'text-align': 'center'
+          }}>Sign in with Spotify</button>
+        </div>
         } 
       </div>
     );
